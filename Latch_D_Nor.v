2b286b0110c47b57d0a4e2970621d037ec6247ef
Elmer Carvalho <elmercarvalhofilho@gmail.com>
2025-10-23 11:44:42 -0300

    refactor: altera o latch SR para o tipo D

--- Latch_D_Nor.v
+++ Latch_D_Nor.v
@@ -1,17 +1,20 @@
-module Latch_SR_Nor (
-    input  wire S,     // Entrada SET
-	 input  wire R,     // Entrada RESET
+module Latch_D_Nor (
+    input  wire D,     // Entrada SET/RESET
     input  wire CLK,   // Entrada CLOCK
     output wire Q,     // Saída principal
     output wire Qn     // Saída negada
 );
 
+    // Cria a inversão para impedir SET e RESET como 11
+    wire Dn;
+	 not(Dn, D);
+
     // Fios internos para os sinais Set (S) e Reset (R)
     wire S_in, R_in;
 	 
 	 // Determina se o SET ou RESET podem ser usados via sinal do CLOCK
-	 and(S_in, S, CLK);
-	 and(R_in, R, CLK);
+	 and(S_in, D, CLK);
+	 and(R_in, Dn, CLK);
 	 
 	 // Lógica de Realimentação
 	 nor(Qn, S_in, Q);
